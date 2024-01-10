59882a73b6542c7fbfc802692af86a0bfed75ae9
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-10 19:56:59 +0600

    ActiveLink setup

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import ActiveLink from "../ActiveLink/ActiveLink";
 
 const Header = () => {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/order">Order</Link>
-        <Link to="/friends">Friends</Link>
-        <Link to="/post">Post</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login">Login</Link>
+        <ActiveLink to="/">Home</ActiveLink>
+        <ActiveLink to="/order">Order</ActiveLink>
+        <ActiveLink to="/friends">Friends</ActiveLink>
+        <ActiveLink to="/post">Post</ActiveLink>
+        <ActiveLink to="/contact">Contact</ActiveLink>
+        <ActiveLink to="/login">Login</ActiveLink>
       </nav>
     </div>
   );
